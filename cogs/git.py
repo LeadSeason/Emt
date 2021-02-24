c578ded0b0b1ca4574141cefd0274d076edfde04
LeadSeason <pauli.tiittanen113@gmail.com>
2021-02-24 20:07:13 +0200

    g

--- cogs/git.py
+++ cogs/git.py
@@ -12,25 +12,26 @@ class git(commands.Cog):
     @commands.command(name="git")
     async def _git(self, ctx):
 
-        try:
-            async with ctx.typing():
-                p = subprocess.Popen(
-                    ["git", "pull"],
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE
-                )
-                p.wait()
-                out, err = p.communicate()
-            embed = discord.Embed(title="Output", description=str(out))
-            jotain = re.findall(r"cogs/.+?.py", str(out))
-            embed.add_field(
-                name="test stuff",
-                value=str(jotain),
-                inline=False
+        # try:
+        async with ctx.typing():
+            p = subprocess.Popen(
+                ["git", "pull"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
             )
-            await ctx.send(embed=embed)
-        except Exception as e:
-            await ctx.send(e)
+            p.wait()
+            out, err = p.communicate()
+        print(out)
+        embed = discord.Embed(title="Output", description=str(out))
+        jotain = re.findall(r"cogs/.+?.py", str(out))
+        embed.add_field(
+            name="test stuff",
+            value=str(jotain),
+            inline=False
+        )
+        await ctx.send(embed=embed)
+        # except Exception as e:
+        #    await ctx.send(e)
 
 
 def setup(bot):
