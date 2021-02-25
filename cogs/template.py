d960f2d203dc33289acecc70b74c61711a8a0020
LeadSeason <pauli.tiittanen113@gmail.com>
2021-02-25 17:44:41 +0200

    a

--- cogs/template.py
+++ cogs/template.py
@@ -10,7 +10,7 @@ class template(commands.Cog):
 
     @commands.command()
     async def template(self, ctx):
-        await ctx.send("aaaaaaaaa")
+        await ctx.send("aaaaaaaaaa")
 
 
 def setup(bot):
