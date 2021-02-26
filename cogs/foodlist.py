749413c7366074d2dd6177a26fd97f539818de5d
LeadSeason <pauli.tiittanen113@gmail.com>
2021-02-26 20:00:37 +0200

    a

--- cogs/foodlist.py
+++ cogs/foodlist.py
@@ -17,76 +17,76 @@ class foodlist(commands.Cog):
         self.bot = bot
 
     async def generate_jsonfile(self):
-        try:
-            url = "https://www.kpedu.fi/palvelut/ravintolat-ja-ruokalistat/menuetti-ja-pikkumenuetti-opiskelijaravintolat"
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as r:
-                    data = await r.read()
-            """
-            data = requests.get(url)
-            """
-            c = Soup(data.content, "html.parser")
-            c = c.find_all("div", class_="content-expanded-list")
-            c = Soup(str(c), "html.parser")
-            foods = c.find_all("p")
+        #try:
+        url = "https://www.kpedu.fi/palvelut/ravintolat-ja-ruokalistat/menuetti-ja-pikkumenuetti-opiskelijaravintolat"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                data = await r.read()
+        """
+        data = requests.get(url)
+        """
+        c = Soup(data.content, "html.parser")
+        c = c.find_all("div", class_="content-expanded-list")
+        c = Soup(str(c), "html.parser")
+        foods = c.find_all("p")
 
-            h = ""
-            for x in foods[1:]:
-                h = h + x.get_text() + "\n"
+        h = ""
+        for x in foods[1:]:
+            h = h + x.get_text() + "\n"
 
-            food_list = list(h.split("\n"))
-            while True:
-                try:
-                    food_list.remove("\xa0")
-                except ValueError:
-                    break
-            food_list.remove("")
+        food_list = list(h.split("\n"))
+        while True:
+            try:
+                food_list.remove("\xa0")
+            except ValueError:
+                break
+        food_list.remove("")
 
-            list_food = []
-            date = ""
-            data = {}
-            for x in food_list:
-                if x.startswith("MAANANTAI"):
+        list_food = []
+        date = ""
+        data = {}
+        for x in food_list:
+            if x.startswith("MAANANTAI"):
 
-                    list_food = []
-                    date = "ma"
-                    list_food.append(x)
-                elif x.startswith("TIISTAI"):
-                    data.update({date: list_food})
-                    list_food = []
-                    date = "ti"
-                    list_food.append(x)
-                elif x.startswith("LASKIAISTIISTAI"):
-                    data.update({date: list_food})
-                    list_food = []
-                    date = "ti"
-                    list_food.append(x)
-                elif x.startswith("KESKIVIIKKO"):
-                    data.update({date: list_food})
-                    list_food = []
-                    date = "ke"
-                    list_food.append(x)
-                elif x.startswith("TORSTAI"):
-                    data.update({date: list_food})
-                    list_food = []
-                    date = "to"
-                    list_food.append(x)
-                elif x.startswith("PERJANTA"):
-                    data.update({date: list_food})
-                    list_food = []
-                    date = "pe"
-                    list_food.append(x)
-                else:
-                    list_food.append(x)
-            data.update({date: list_food})
+                list_food = []
+                date = "ma"
+                list_food.append(x)
+            elif x.startswith("TIISTAI"):
+                data.update({date: list_food})
+                list_food = []
+                date = "ti"
+                list_food.append(x)
+            elif x.startswith("LASKIAISTIISTAI"):
+                data.update({date: list_food})
+                list_food = []
+                date = "ti"
+                list_food.append(x)
+            elif x.startswith("KESKIVIIKKO"):
+                data.update({date: list_food})
+                list_food = []
+                date = "ke"
+                list_food.append(x)
+            elif x.startswith("TORSTAI"):
+                data.update({date: list_food})
+                list_food = []
+                date = "to"
+                list_food.append(x)
+            elif x.startswith("PERJANTA"):
+                data.update({date: list_food})
+                list_food = []
+                date = "pe"
+                list_food.append(x)
+            else:
+                list_food.append(x)
+        data.update({date: list_food})
 
-            with open("./data/foods.json", 'w', encoding='utf8') as f:
-                json.dump(data, f, ensure_ascii=False)
-                # json.dump(data, f, indent=4, ensure_ascii=False)
-            return "success"
-        except Exception as e:
-            print(e)
-            return "error"
+        with open("./data/foods.json", 'w', encoding='utf8') as f:
+            json.dump(data, f, ensure_ascii=False)
+            # json.dump(data, f, indent=4, ensure_ascii=False)
+        return "success"
+        # except Exception as e:
+        #    print(e)
+        #    return "error"
 
     @commands.command(aliases=["fl", "sapuska"])
     async def foodlist(self, ctx, *args):
