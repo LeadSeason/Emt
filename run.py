0888b03de02ee6668c3edf9722c3cbc03b99bdc6
LeadSeason <pauli.tiittanen113@gmail.com>
2021-02-26 16:41:55 +0200

    s

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-from bot_copy import bot
+import logging as log
+import subprocess
+import json
 
+
+def get_conf(data):
+    with open("./conf/github.conf.json") as discord_conf:
+        return json.load(discord_conf)[data]
+
+
+log.basicConfig(format='%(levelname)s:%(message)s')
+try:
+    from bot import bot
+except ModuleNotFoundError:
+    log.error("Bot isn't installed")
+    log.info("installing...")
+    repo = "https://" + get_conf("username") + ":" + get_conf("token") + "@" + get_conf("repo")
+    p = subprocess.Popen(["git", "clone", repo, "."])
+    p.wait()
+
+from bot import bot
+p = subprocess.Popen(["git", "pull"])
+p.wait()
 bot()
