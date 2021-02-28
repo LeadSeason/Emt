9b599828e0c2be32a71d8d10e24f01fcd97b857e
LeadSeason <pauli.tiittanen113@gmail.com>
2021-02-28 11:01:10 +0200

    a

--- utils/ytapi.py
+++ utils/ytapi.py
@@ -7,7 +7,7 @@ count = 1
 API_KEY = 'AIzaSyCC4X40Bx7oju-xftdKm9qPXnqfp6IY7dk'
 random = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
 
-urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(API_KEY,count,random)
+urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(API_KEY, count, random)
 webURL = urllib.request.urlopen(urlData)
 data = webURL.read()
 encoding = webURL.info().get_content_charset('utf-8')
@@ -15,5 +15,4 @@ results = json.loads(data.decode(encoding))
 
 for data in results['items']:
     videoId = (data['id']['videoId'])
-    print("https://www.youtube.com/watch?v=" + videoId)
-    #store your ids
+    print("https://youtu.be/" + videoId)
