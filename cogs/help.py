39b5ec91eda4ee22004675527cddc23685e4dd68
LeadSeason <pauli.tiittanen113@gmail.com>
2021-03-15 10:01:06 +0200

    progress on help command

--- cogs/help.py
+++ cogs/help.py
@@ -14,25 +14,32 @@ class help(commands.Cog):
         if ctx.invoked_subcommand is None:
             if arg is None:
                 arg = "help"
+            arg = arg.lower()
 
-            try:
-                with open("./data/help.json", encoding='utf-8') as s:
-                    d = json.load(s)
-            except FileNotFoundError:
-                await ctx.send("no help.json found")
+            if arg in self.bot.commands:
+                try:
+                    with open("./data/help.json", encoding='utf-8') as s:
+                        d = json.load(s)[arg]
+                except FileNotFoundError:
+                    await ctx.send("no help.json found")
+                except KeyError:
+                    print(f"help for command {arg} doesnt exist")
+                    await ctx.send("Help for this command doesn't exist")
+                else:
+                    embed = discord.Embed(title="Help")
+                    embed.add_field(
+                        name="description",
+                        value=d["description"],
+                        inline=False
+                    )
+                    embed.add_field(
+                        name="usage",
+                        value=d["usage"],
+                        inline=False
+                    )
+                    await ctx.send(embed=embed)
             else:
-                embed = discord.Embed(title="Help")
-                embed.add_field(
-                    name="description",
-                    value=d["description"],
-                    inline=False
-                )
-                embed.add_field(
-                    name="usage",
-                    value=d["usage"],
-                    inline=False
-                )
-                await ctx.send(embed=embed)
+                await ctx.send("Command doesn't exist")
 
     @help.command()
     @commands.is_owner()
