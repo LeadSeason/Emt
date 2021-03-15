444690db13836babd7c65ee4a738cc8e82bf052e
LeadSeason <pauli.tiittanen113@gmail.com>
2021-03-15 15:31:06 +0200

    joo?

--- cogs/help.py
+++ cogs/help.py
@@ -139,17 +139,18 @@ class help(commands.Cog):
                 }
             await ctx.send(_help)
             """
-            with open("./data/help.json", "r+") as f:
+            with open("./data/help.json", "r+", encoding='utf8') as f:
                 data = json.loads(f.read())
                 data.update(_help)
                 f.seek(0)
-                json.dump(data, f, indent=4)
+                json.dump(data, f, indent=4, ensure_ascii=False)
+                f.truncate()
             """
-            with open("./data/help.json", "r") as f:
+            with open("./data/help.json", "r", encoding="utf8") as f:
                 data = json.load(f)
-            data = data.update(_help)
-            with open("./data/help.json", 'w') as f:
-                json.dump(data, f, indent=4)
+            data.update(_help)
+            with open("./data/help.json", 'w', encoding='utf8') as f:
+                json.dump(data, f, indent=4, ensure_ascii=False)
 
     @commands.command(name="commands")
     async def idkjotain(self, ctx):
