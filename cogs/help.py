334d56593f7bc0c90aa439090325e851dc218256
LeadSeason <pauli.tiittanen113@gmail.com>
2021-03-15 10:26:34 +0200

    ?

--- cogs/help.py
+++ cogs/help.py
@@ -12,34 +12,38 @@ class help(commands.Cog):
     @commands.group(description="Returns all commands available")
     async def help(self, ctx, arg=None):
         if ctx.invoked_subcommand is None:
+            commands_list = []
+            for x in self.bot.commands:
+                commands_list.append(x)
+
             if arg is None:
                 arg = "help"
             arg = arg.lower() + " "
-            for command in self.bot.commands:
-                if str(arg) is str(command):
-                    try:
-                        with open("./data/help.json", encoding='utf-8') as s:
-                            d = json.load(s)[arg]
-                    except FileNotFoundError:
-                        await ctx.send("no help.json found")
-                    except KeyError:
-                        print(f"help for command {arg} doesnt exist")
-                        await ctx.send("Help for this command doesn't exist")
-                    else:
-                        embed = discord.Embed(title="Help")
-                        embed.add_field(
-                            name="description",
-                            value=d["description"],
-                            inline=False
-                        )
-                        embed.add_field(
-                            name="usage",
-                            value=d["usage"],
-                            inline=False
-                        )
-                        await ctx.send(embed=embed)
 
-                    break
+            if arg in commands_list:
+                try:
+                    with open("./data/help.json", encoding='utf-8') as s:
+                        d = json.load(s)[arg]
+                except FileNotFoundError:
+                    await ctx.send("no help.json found")
+                except KeyError:
+                    print(f"help for command {arg} doesnt exist")
+                    await ctx.send("Help for this command doesn't exist")
+                else:
+                    embed = discord.Embed(title="Help")
+                    embed.add_field(
+                        name="description",
+                        value=d["description"],
+                        inline=False
+                    )
+                    embed.add_field(
+                        name="usage",
+                        value=d["usage"],
+                        inline=False
+                    )
+                    await ctx.send(embed=embed)
+
+                
             else:
                 await ctx.send("Command doesn't exist")
 
