4e00909facdd4a87125e9c6bfb3bef955b8d80cf
LeadSeason <pauli.tiittanen113@gmail.com>
2021-03-15 09:41:56 +0200

    a

--- cogs/help.py
+++ cogs/help.py
@@ -10,14 +10,33 @@ class help(commands.Cog):
         self.bot = bot
 
     @commands.group(description="Returns all commands available")
-    async def help(self, ctx, arg):
+    async def help(self, ctx, arg=None):
         if ctx.invoked_subcommand is None:
-            await ctx.send(arg)
+            await ctx.send()
             """
             _list = self.bot.commands
             with open("./data/help.json", encoding='utf-8') as s:
                 _help = json.load(s)
             """
+        elif arg is None:
+            embed = discord.Embed(title="Help")
+            try:
+                with open("./data/help.json", encoding='utf-8') as s:
+                    d = json.load(s)
+            except FileNotFoundError:
+                await ctx.send("no help.json found")
+            else:
+                embed.add_field(
+                    name="description",
+                    value=d["description"],
+                    inline=False
+                    )
+                embed.add_field(
+                    name="usage",
+                    value=d["usage"],
+                    inline=False
+                    )
+                await ctx.send(embed=embed)
 
     @help.command()
     @commands.is_owner()
